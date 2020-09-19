19038f77eb1e0f5d6ac5db7067061fcc73231c66
toraring <katatj@naver.com>
2020-09-19 16:24:04 +0900

    Create React Project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="gray-background">
+      <img src={logo} lat="Cooodddding" />
+      <h2>코딩은 어렵다
+      </h2>
+    
+    </div>
+  );
+}
+
+export default App;
